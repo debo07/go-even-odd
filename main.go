df61429ca00e0e341a65f022033c3629d0862930
Debajit Majumder <debajit.business@gmail.com>
2023-12-27 22:05:05 +0530

    Used make func to create a slice of int of length 10, then filled the slice with values. Next loop determines the number is odd or even. - Just another approach

--- main.go
+++ main.go
@@ -3,7 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	numbers := make([]int, 10)
+
+	// First create the slice by iterating
+	for i := range(numbers) {
+		numbers[i] = i + 1
+	}
 
 	for _, number := range(numbers) {
 		if number % 2 == 0 {
